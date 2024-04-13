0695e81ab91eb425f05e1357604be7cb2c1fa952
Nemzytch <lesieutre.kevin@gmail.com>
2024-04-13 21:13:10 +0200

    azefazef

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     print('Hello, World!')
     return 'Hello, World!'
+@app.route('/hello')
+def hello():
+    print('Hello, World!')
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(port=5684)
