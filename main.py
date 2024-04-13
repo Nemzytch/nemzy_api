3bfa6f77291c6a5c5c12c904ccdc29e90ef7f48e
Nemzytch <lesieutre.kevin@gmail.com>
2024-04-13 21:46:30 +0200

    triple!

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     print('Hello, World!')
-    return 'Hello, World!'
+    return 'Hello, World!!!'
 
 @app.route('/hello')
 def hello():
     print('Hello, World!')
-    return 'Hello, World!'
+    return 'Hello, World!!!'
 
 if __name__ == '__main__':
     app.run(port=5684, debug=True)
